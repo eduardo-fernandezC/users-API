528f47770bf3701b10dab31163c7159552bc6dc3
Eduardo Fernández <ed.fernandezc@duocuc.cl>
2025-12-13 22:35:08 -0300

    Agrega ejemplos y descripciones a Rol

--- src/main/java/com/example/usuarios/cl/usuarios/model/Rol.java
+++ src/main/java/com/example/usuarios/cl/usuarios/model/Rol.java
@@ -1,5 +1,6 @@
 package com.example.usuarios.cl.usuarios.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -15,12 +16,22 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Entidad que representa el rol del usuario en el sistema")
 public class Rol {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(
+        description = "ID unico del rol",
+        example = "1",
+        accessMode = Schema.AccessMode.READ_ONLY
+    )
     private Long id;
 
     @Column(nullable = false, length = 40)
+    @Schema(
+        description = "Nombre del rol",
+        example = "ADMIN"
+    )
     private String nombre;
 }
